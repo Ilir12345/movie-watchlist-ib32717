25d0ff67e23c9ed4f90d66c0cdf7604d2c60ea72
ilir12345 <ib32717@seeu.edu.mk>
2026-06-02 01:21:18 +0200

    Implemented Read

--- src/utils/movies.js
+++ src/utils/movies.js
@@ -1,7 +1,7 @@
 export const movies = [
   { id: 1, title: 'Inception', director: 'Christopher Nolan', genre: 'Sci-Fi', watched: true },
   { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola', genre: 'Crime', watched: false },
-  { id: 3, title: 'Interstellar', director: 'Christopher Nolan', genre: 'Sci-Fi', watched: true },
-  { id: 4, title: 'Parasite', director: 'Bong Joon-ho', genre: 'Thriller', watched: false },
-  { id: 5, title: 'The Dark Knight', director: 'Christopher Nolan', genre: 'Action', watched: true },
+  { id: 3, title: 'Parasite', director: 'Bong Joon-ho', genre: 'Thriller', watched: true },
+  { id: 4, title: 'Spirited Away', director: 'Hayao Miyazaki', genre: 'Animation', watched: false },
+  { id: 5, title: 'Mad Max: Fury Road', director: 'George Miller', genre: 'Action', watched: false },
 ]
